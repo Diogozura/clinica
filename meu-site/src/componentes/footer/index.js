f584051e03cc275c518fd8a7d686980e8f31fefb
Diogo Zura <65250673+Diogozura@users.noreply.github.com>
2025-09-29 09:47:13 -0300

    Update index.js

--- meu-site/src/componentes/footer/index.js
+++ meu-site/src/componentes/footer/index.js
@@ -67,7 +67,7 @@ export default function Footer() {
           <Grid size={{ xs: 12, md: 6 }} order={{ xs: 1, md: 2 }}>
             <Contato />
           </Grid>
-          <Grid size={12} textAlign={"center"} display={'flex'} flexWrap={'wrap'} justifyContent={'space-evenly'} marginBottom={{ xs: 0, md: 5}} order={{ xs: 3, md: 3 }} color="primary.contrastText">
+          <Grid size={12} textAlign={"center"} display={'flex'} flexWrap={'wrap'} justifyContent={'space-evenly'} marginBottom={{ xs: 5, md: 5}} order={{ xs: 3, md: 3 }} color="primary.contrastText">
            <Typography variant="body2"  component={'p'}>© 2021-2025 Cotidente</Typography> 
            <Typography variant="body2"  component={'p'}> <Link href="/politica-privacidade">Política de privacidade</Link></Typography> 
           </Grid>
